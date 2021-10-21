710848f53015f81795cac96f60397f00bb8ae991
Abbas Lamouri <abbaslamouri@yrlus.com>
2021-10-21 07:41:17 -0400

    Initial commit

---
+++ layouts/custom.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <header>Header1</header>
+    <slot />
+    <footer>Footer1</footer>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<script setup>
+const heading = 'Hello from About page'
+const pageTitle = 'About Page'
+const pageDescription = 'About page descriptio'
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ heading }}</h1>
+    <Head lang="en-US">
+      <Title>{{ pageTitle }}</Title>
+      <Meta name="description" :content="`${pageDescription}`" />
+      <Link rel="preload" href="#" as="script" />
+    </Head>
+  </div>
+</template>
+
+<style></style>
