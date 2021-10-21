75c14a39122e414d0bc82d72bc0a82fdc3fe3cf4
Abbas Lamouri <abbas@Abbas-Omen.GENVAC.local>
2021-10-21 10:13:58 -0400

    Modofied api/users/index

--- api/users/index.js
+++ api/users/index.js
@@ -3,22 +3,18 @@ import express from 'express'
 // Create express instnace
 const app = express()
 
-let xx
-
 // Init body-parser options (inbuilt with express)
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.all("/", (req, res, next) => {
-  xx=2345
-  console.log("serverMiddleware", req.body)
-  app.xxx = 12345
-  // return {data:123345}
-  next()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.all('/', (req, res, next) => {
+	xx = 2345
+	console.log('serverMiddleware', req.body)
+	// return {data:123345}
+	next()
 })
 
 // Export the server middleware
-export default  app
-
+export default app
 
 // export default (req, res, next) => {
 //   console.log("HHHHHHHHHH", req.url)
