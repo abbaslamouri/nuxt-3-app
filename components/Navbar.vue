6e251526aecb249cccd8469326432bffee7004a2
Abbas Lamouri <abbas@Abbas-Omen.GENVAC.local>
2021-10-22 14:14:58 -0400

    M

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-const router = useRouter()
-const navLinks = router.getRoutes()
-console.log(navLinks)
+	const router = useRouter()
+	const navLinks = router.getRoutes()
+	console.log(navLinks)
 </script>
 
 <template>
-  <div class="navbar">
-    <ul>
-      <li v-for="link in navLinks" :index="link.path">
-        <NuxtLink :to="link.path">{{ link }}</NuxtLink>
-      </li>
-    </ul>
-  </div>
+	<div class="navbar">
+		<ul>
+			<li v-for="link in navLinks" :index="link.path">
+				<NuxtLink :to="link.path">{{ link.name }}</NuxtLink>
+			</li>
+		</ul>
+	</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,45 @@
 <script setup lang="ts">
-// import { ref, onMounted } from 'vue'
-import axios from 'axios'
+	// import { ref, onMounted } from 'vue'
+	import axios from 'axios'
 
-const heading = ref('Hello from the Home page')
-const pageTitle = 'Home Page'
-const pageDescription = 'page description'
+	const heading = ref('Hello from the Home page')
+	const pageTitle = 'Home Page'
+	const pageDescription = 'page description'
 
-const fetchUsers = async () => {
-  // const instance = axios.create({
-  //   baseURL: 'http://localhost:3000/api',
-  //   timeout: 5000,
-  //   data: {xyz: 12345}
-  //   // headers: {'X-Custom-Header': 'foobar'}
-  // })
-  const response = await axios({
-    method: 'post',
-    url: 'http://localhost:3000/api/users',
-    data: { name: 'Abbas Lamouri', email: 'abbaslamouri@yrlus.com' },
-  })
-  console.log(response)
-}
+	const fetchUsers = async () => {
+		// const instance = axios.create({
+		//   baseURL: 'http://localhost:3000/api',
+		//   timeout: 5000,
+		//   data: {xyz: 12345}
+		//   // headers: {'X-Custom-Header': 'foobar'}
+		// })
+		const response = await axios({
+			method: 'post',
+			url: 'http://localhost:3000/api/users',
+			data: { name: 'Abbas Lamouri', email: 'abbaslamouri@yrlus.com' },
+		})
+		console.log(response)
+	}
 </script>
 
 <script lang="ts">
-export default {
-  // layout: 'custom',
-}
+	export default {
+		// layout: 'custom',
+	}
 </script>
 
 <template>
-  <div id="app">
-    <h1>{{ heading }}</h1>
-    <!-- <h1>{{ router }}</h1> -->
-    <Head lang="en-US">
-      <Title>{{ pageTitle }}</Title>
-      <Meta name="description" :content="`${pageDescription}`" />
-      <Link rel="preload" href="#" as="script" />
-    </Head>
+	<div id="app">
+		<h1>{{ heading }}</h1>
+		<!-- <h1>{{ router }}</h1> -->
+		<Head lang="en-US">
+			<Title>{{ pageTitle }}</Title>
+			<Meta name="description" :content="`${pageDescription}`" />
+			<Link rel="preload" href="#" as="script" />
+		</Head>
 
-    <button @click="fetchUsers">Fetch Users</button>
-  </div>
+		<button @click="fetchUsers">Fetch Users</button>
+	</div>
 </template>
 
 <style></style>
